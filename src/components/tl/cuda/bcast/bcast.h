a62ec58248bad046cf1f8bf8e138ed82f91b47f2
Ilya Kryukov <ilyakrukov@gmail.com>
2024-12-19 17:05:27 +0100

    TL/CUDA: initial skeleton

--- src/components/tl/cuda/bcast/bcast.h
+++ src/components/tl/cuda/bcast/bcast.h
@@ -13,13 +13,15 @@
 enum
 {
     UCC_TL_CUDA_BCAST_ALG_LINEAR,
+    UCC_TL_CUDA_BCAST_ALG_CE,
     UCC_TL_CUDA_BCAST_ALG_LAST
 };
 
 extern ucc_base_coll_alg_info_t
     ucc_tl_cuda_bcast_algs[UCC_TL_CUDA_BCAST_ALG_LAST + 1];
 
-#define UCC_TL_CUDA_BCAST_DEFAULT_ALG_SELECT_STR "bcast:cuda:@0"
+// TODO: change to 0 after dev
+#define UCC_TL_CUDA_BCAST_DEFAULT_ALG_SELECT_STR "bcast:cuda:@1"
 
 ucc_status_t ucc_tl_cuda_bcast_init(ucc_base_coll_args_t *coll_args,
                                     ucc_base_team_t      *tl_team,
@@ -29,8 +31,13 @@ ucc_status_t ucc_tl_cuda_bcast_linear_init(ucc_base_coll_args_t *coll_args,
                                            ucc_base_team_t      *tl_team,
                                            ucc_coll_task_t     **task_p);
 
+ucc_status_t ucc_tl_cuda_bcast_ce_init(ucc_base_coll_args_t *coll_args,
+                                           ucc_base_team_t      *tl_team,
+                                           ucc_coll_task_t     **task_p);
+
 static inline int ucc_tl_cuda_bcast_alg_from_str(const char *str)
 {
+    ucc_info("algo selection: %s\n", str);
     int i;
     for (i = 0; i < UCC_TL_CUDA_BCAST_ALG_LAST; i++) {
         if (0 == strcasecmp(str, ucc_tl_cuda_bcast_algs[i].name)) {
